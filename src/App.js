24f1b29f0ac86c6943d13bf593cc399eca9f03dc
Eduard Vallvé <66818781+eduvallve@users.noreply.github.com>
2024-06-04 12:24:59 +0200

    React styled library

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`;
+
+const Content = styled.div`
+  padding: 20px 25px;
+`;
+
+const Button = styled.button`
+  background-color: ${(props) => (props.primary ? "red" : "white")};
+  color: ${(props) => (props.primary ? "white" : "red")};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+  transition: box-shadow 0.25s ease;
+
+  &:hover {
+    box-shadow: 1px 2px 5px rgb(0, 0, 0, 0.3);
+  }
+
+  &.secondary {
+    background-color: blue;
+    border-color: blue;
+    color: white;
+  }
+`;
+
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+  margin: 10px 0 0;
+`;
+
+const Link = ({ className, ...props }) => {
+  return (
+    // eslint-disable-next-line jsx-a11y/anchor-has-content
+    <a className={className} {...props}></a>
+  );
+};
+
+const StyledLink = styled(Link)`
+  color: blue;
+`;
+
+const Input = styled.input.attrs((props) => ({
+  type: "text",
+  color: props.color || "red",
+}))`
+  font-size: 20px;
+  border 1px solid red;
+  display:block;
+  margin: 20px 0 0;
+  color: ${(props) => props.color}
+`;
+
+const Password = styled(Input).attrs({ type: "password" })`
+  margin: 10px 0 0
+`;
+
+const girar = keyframes`
+  from {
+    transform:rotate(0deg);
+  }
+  to {
+    transform:rotate(360deg);
+  }
+`;
+
+const Rotar = styled.div`
+  animation: ${girar} 2s linear infinite;
+  display: inline-block;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Content>
+      <P>Hola, soy un párrafo</P>
+      <Button primary="true">Botón primario</Button>{" "}
+      <Button>Botón secundario</Button>
+      <BlockButton className="secondary">Enviar</BlockButton>
+      <BlockButton as="a" href="#">
+        Enviar
+      </BlockButton>
+      <Link>Link</Link>
+      <StyledLink>Link con estilo</StyledLink>
+      <Input />
+      <Password color="brown" />
+      <br />
+      <Rotar>Estoy girando</Rotar>
+    </Content>
   );
 }
 
